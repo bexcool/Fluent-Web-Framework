642a1fc48ac6f4e74752e461bdd54de5b8623396
Pietro228 <53868994+Pietro228@users.noreply.github.com>
2022-02-04 19:08:41 +0100

    Completed password box, fix svgs

--- src/js/fluent_functions.js
+++ src/js/fluent_functions.js
@@ -450,8 +450,6 @@ function MakeFluentElements() {
     let doc = document.documentElement;
 
     for (const code of document.querySelectorAll("code")) {
-        let codeAtt = Object.assign({}, Array.from(code.attributes, ({name, value}) => ({[name]: value})));
-        
         code.outerHTML =    '<div class="fluent-code-container">' +
                             '<div style="display: inline-block; margin: 0.225em 0;"><code ' + 
                             AttributesToString(code) +
@@ -459,7 +457,7 @@ function MakeFluentElements() {
                             code.innerHTML + 
                             '</code></div>' +
                             '<button class="fluent-code-copy-button" onclick="CopyToClipboard(\'' + code.innerHTML + '\');">' + 
-                            '<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">' +
+                            '<svg class="fluent-svg" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">' +
                             '<path class="fluent-svg-path" d="M8 2C6.89543 2 6 2.89543 6 4V14C6 15.1046 6.89543 16 8 16H14C15.1046 16 16 15.1046 16 14V4C16 2.89543 15.1046 2 14 2H8ZM7 4C7 3.44772 7.44772 3 8 3H14C14.5523 3 15 3.44772 15 4V14C15 14.5523 14.5523 15 14 15H8C7.44772 15 7 14.5523 7 14V4ZM4 6.00001C4 5.25973 4.4022 4.61339 5 4.26758V14.5C5 15.8807 6.11929 17 7.5 17H13.7324C13.3866 17.5978 12.7403 18 12 18H7.5C5.567 18 4 16.433 4 14.5V6.00001Z" fill="#212121"/>' +
                             '</svg>' +
                             '</button>' +
@@ -517,7 +515,7 @@ function MakeFluentElements() {
     for (const input_password of inputs_password) {
         input_password.outerHTML =  '<div class="fluent-textbox-container">' +
                                 '<input ' + AttributesToString(input_password) + ' >' + 
-                                '<button class="fluent-show-password-input-button" style="opacity: 0; display: none;"><img class="fluent-expander-arrow" src="https://resources.bexcool.eu/fluentstyle/src/img/eye_regular.svg"></button>' +
+                                '<a class="fluent-show-password-input" style="opacity: 0; display: none;"><svg style="margin-top: 0.425em;" width="20" height="20" viewBox="0 0 20 20"xmlns="http://www.w3.org/2000/svg"><path class="fluent-svg-path" d="M3.25909 11.6021C3.94254 8.32689 6.79437 6 10 6C13.2057 6 16.0574 8.32688 16.7409 11.6021C16.7974 11.8725 17.0622 12.0459 17.3325 11.9895C17.6029 11.933 17.7763 11.6682 17.7199 11.3979C16.9425 7.67312 13.6934 5 10 5C6.3066 5 3.05742 7.67311 2.28017 11.3979C2.22377 11.6682 2.39718 11.933 2.6675 11.9895C2.93782 12.0459 3.20268 11.8725 3.25909 11.6021ZM10 8C8.067 8 6.5 9.567 6.5 11.5C6.5 13.433 8.067 15 10 15C11.933 15 13.5 13.433 13.5 11.5C13.5 9.567 11.933 8 10 8ZM7.5 11.5C7.5 10.1193 8.61929 9 10 9C11.3807 9 12.5 10.1193 12.5 11.5C12.5 12.8807 11.3807 14 10 14C8.61929 14 7.5 12.8807 7.5 11.5Z" fill="#212121"/></svg></a>' +
                                 '</div>';
     }
 
@@ -534,11 +532,11 @@ function MakeFluentElements() {
         button.addEventListener("click", () => {
             if (input_password.getAttribute("type") == "password") {
                 input_password.setAttribute("type", "text");
-                button.innerHTML = '<img class="fluent-expander-arrow" src="https://resources.bexcool.eu/fluentstyle/src/img/eye_filled.svg">';
+                button.innerHTML = '<svg style="margin-top: 0.425em;" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path class="fluent-svg-path" d="M3.25909 11.6021C3.94254 8.32689 6.79437 6 10 6C13.2057 6 16.0574 8.32688 16.7409 11.6021C16.7974 11.8725 17.0622 12.0459 17.3325 11.9895C17.6029 11.933 17.7763 11.6682 17.7199 11.3979C16.9425 7.67312 13.6934 5 10 5C6.3066 5 3.05742 7.67311 2.28017 11.3979C2.22377 11.6682 2.39718 11.933 2.6675 11.9895C2.93782 12.0459 3.20268 11.8725 3.25909 11.6021ZM9.98953 8C11.9225 8 13.4895 9.567 13.4895 11.5C13.4895 13.433 11.9225 15 9.98953 15C8.05653 15 6.48953 13.433 6.48953 11.5C6.48953 9.567 8.05653 8 9.98953 8Z" fill="#212121"/></svg>';
             }
             else {
                 input_password.setAttribute("type", "password");
-                button.innerHTML = '<img class="fluent-expander-arrow" src="https://resources.bexcool.eu/fluentstyle/src/img/eye_regular.svg">';
+                button.innerHTML = '<svg style="margin-top: 0.425em;" width="20" height="20" viewBox="0 0 20 20"xmlns="http://www.w3.org/2000/svg"><path class="fluent-svg-path" d="M3.25909 11.6021C3.94254 8.32689 6.79437 6 10 6C13.2057 6 16.0574 8.32688 16.7409 11.6021C16.7974 11.8725 17.0622 12.0459 17.3325 11.9895C17.6029 11.933 17.7763 11.6682 17.7199 11.3979C16.9425 7.67312 13.6934 5 10 5C6.3066 5 3.05742 7.67311 2.28017 11.3979C2.22377 11.6682 2.39718 11.933 2.6675 11.9895C2.93782 12.0459 3.20268 11.8725 3.25909 11.6021ZM10 8C8.067 8 6.5 9.567 6.5 11.5C6.5 13.433 8.067 15 10 15C11.933 15 13.5 13.433 13.5 11.5C13.5 9.567 11.933 8 10 8ZM7.5 11.5C7.5 10.1193 8.61929 9 10 9C11.3807 9 12.5 10.1193 12.5 11.5C12.5 12.8807 11.3807 14 10 14C8.61929 14 7.5 12.8807 7.5 11.5Z" fill="#212121"/></svg>';
             }
         });
     }
