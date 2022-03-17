0a6af05018c4c90cf144923164a8e7ae60ba5400
Petr Pavlík <53868994+Pietro228@users.noreply.github.com>
2022-03-17 11:09:39 +0100

    Txtbox is now focused after clearing text

--- src/js/fluent_functions.js
+++ src/js/fluent_functions.js
@@ -645,16 +645,16 @@ function SetActivePageIndex(page_switcher_id, index) {
 }
 
 function InitFluentElements() {
-    // Code
     let doc = document.documentElement;
 
+    // Code
     for (const code of document.querySelectorAll("code")) {
         code.outerHTML =    '<div class="fluent-code-container">' +
                             '<div style="display: inline-block; margin: 0.225em 0;"><code ' + 
                             AttributesToString(code) +
                             ' style="' + code.style.cssText + '">' +
                             code.innerHTML + 
-                            '</code></div>' + /*onclick="CopyToClipboard(\'' + code.innerHTML + '\')"*/
+                            '</code></div>' +
                             '<button class="fluent-code-copy-button" >' + 
                             '<svg class="fluent-svg" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">' +
                             '<path class="fluent-svg-path" d="M8 2C6.89543 2 6 2.89543 6 4V14C6 15.1046 6.89543 16 8 16H14C15.1046 16 16 15.1046 16 14V4C16 2.89543 15.1046 2 14 2H8ZM7 4C7 3.44772 7.44772 3 8 3H14C14.5523 3 15 3.44772 15 4V14C15 14.5523 14.5523 15 14 15H8C7.44772 15 7 14.5523 7 14V4ZM4 6.00001C4 5.25973 4.4022 4.61339 5 4.26758V14.5C5 15.8807 6.11929 17 7.5 17H13.7324C13.3866 17.5978 12.7403 18 12 18H7.5C5.567 18 4 16.433 4 14.5V6.00001Z" fill="#212121"/>' +
@@ -670,9 +670,7 @@ function InitFluentElements() {
 
             ShowFlyout(code.parentElement.parentElement.children[1], "Code copied!", 1000);
         });
-    }
 
-    for (const code of document.querySelectorAll("code")) {
         if (code.hasAttribute("sh")) CodeSyntaxHigh(code, code.getAttribute("sh").toLowerCase());
     }
 
@@ -712,6 +710,7 @@ function InitFluentElements() {
 
         input_text.parentElement.lastElementChild.addEventListener("click", () => {
             input_text.value = "";
+            setTimeout(() => input_text.focus());
         });
     }
 
