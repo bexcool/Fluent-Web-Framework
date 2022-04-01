8e4da62fcb6d50c0006ba6449113f7001d64698d
Spejik <Jiriksperlik@seznam.cz>
2022-04-01 16:59:20 +0200

    Fix copy to clipboard

--- src/js/elements/code/init.ts
+++ src/js/elements/code/init.ts
@@ -1,6 +1,7 @@
 import { getIcon } from "../../icons";
 import { copyToClipboard } from "../../util/clipboard";
 import { attributesToString, decodeHtml } from "../../util/html";
+import { uniqueRand } from "../../util/math";
 import { showFlyout } from "../flyout/flyout";
 import { codeSyntaxHigh } from "./code";
 
@@ -8,15 +9,18 @@ export default () => {
 	const codes = document.querySelectorAll("code");
 
 	codes.forEach(code => {
-		code.outerHTML = `<div class="fluent-code-container"><div style="display: inline-block; margin: 0.225em 0;"><code ${attributesToString(code)} style="${code.style.cssText}">${code.innerHTML}</code></div><button class="fluent-code-copy-button" >${getIcon("clipboard")}</button></div>`;
+		const copyId = `flcode-${code.id || "cat"}${uniqueRand()}`;
+
+		code.outerHTML = `<div class="fluent-code-container"><div style="display: inline-block; margin: 0.225em 0;"><code ${attributesToString(code)} style="${code.style.cssText}">${code.innerHTML}</code></div><button class="fluent-code-copy-button" id="${copyId}">${getIcon("clipboard")}</button></div>`;
 
 		// Copy button click
-		code?.parentElement?.parentElement?.children[1].addEventListener("click", () => {
+		const copyButton = document.getElementById(copyId) as HTMLButtonElement;
+		copyButton.addEventListener("click", () => {
 			// eslint-disable-next-line no-irregular-whitespace
 			copyToClipboard(decodeHtml(code.innerHTML).replace(/<\/?span[^>]*>/g, "").replace(/[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g, " "));
 
-			if (code?.parentElement?.parentElement?.children[1])
-				showFlyout(code.parentElement.parentElement.children[1] as HTMLElement, "Code copied!", 1000);
+			if (copyButton)
+				showFlyout(copyButton, "Code copied!", 1000);
 		});
 
 		const codeContent = code.innerHTML;
--- src/js/util/math.ts
+++ src/js/util/math.ts
@@ -1,3 +1,9 @@
 export const clamp = (num: number, min: number, max: number) => {
 	return Math.min(Math.max(num, min), max);
 };
+
+export const uniqueRand = () => {
+	const date = Date.now();
+	const num = Math.random();
+	return Math.floor(date * num);
+};
