b3cd531cd3a660d214705afff0f44754693bc1a2
Spejik <Jiriksperlik@seznam.cz>
2022-04-05 23:09:36 +0200

    Add fluent-icon element

--- src/js/icons/index.ts
+++ src/js/icons/index.ts
@@ -1,14 +1,27 @@
+import { FluentElement, FluentRegister } from "../elements/fluent-element";
 import { FluentDefine, FluentExpose } from "../fluent";
 import { _icons, _Icons, _iconsUrl } from "./list";
 export { _Icons as Icons } from "./list";
 
 const _cache: { [k in _Icons]?: string } = {};
 
+// Only registered in this module's init function
+class Icon extends HTMLElement implements FluentElement {
+	connectedCallback() {
+		setTimeout(async () => {
+			const icon = this.innerText;
+			if (iconExists(icon))
+				this.outerHTML = `<div class="fluent-icon">${await getIcon(icon)}</div>`;
+		});
+	}
+}
+
 export default () => {
 	FluentDefine(_icons, "icons", true);
 	FluentDefine(_iconsUrl, "CDN_URL_ICONS", true);
 	FluentExpose(getIcon, true);
 	FluentExpose(iconExists, true);
+	FluentRegister(Icon, "fluent-icon");
 };
 
 export const iconExists = (icon: string): icon is _Icons => icon in _icons;
